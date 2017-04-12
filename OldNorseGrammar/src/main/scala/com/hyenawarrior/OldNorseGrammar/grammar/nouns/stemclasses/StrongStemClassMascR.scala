915d90c1908f61d2ecbbd67c686d52533f5f09ae
HyenaWarrior <andras.piroska.88@gmail.com>
2017-04-12 22:09:52 +0200

    Fix Strong Masc.-R class of Nouns

--- OldNorseGrammar/src/main/scala/com/hyenawarrior/OldNorseGrammar/grammar/nouns/stemclasses/StrongStemClassMascR.scala
+++ OldNorseGrammar/src/main/scala/com/hyenawarrior/OldNorseGrammar/grammar/nouns/stemclasses/StrongStemClassMascR.scala
@@ -2,22 +2,29 @@ package com.hyenawarrior.OldNorseGrammar.grammar.nouns.stemclasses
 
 import com.hyenawarrior.OldNorseGrammar.grammar.Case._
 import com.hyenawarrior.OldNorseGrammar.grammar.Number._
-import com.hyenawarrior.OldNorseGrammar.grammar.{Case, Number}
+import com.hyenawarrior.OldNorseGrammar.grammar.{Case, I_Umlaut, Number, WordTransformation}
 
 /**
 	* Created by HyenaWarrior on 2017.04.09..
 	*/
 object StrongStemClassMascR extends NounStemClass
 {
+	override def transformationsFor(decl: (Number, Case)): List[WordTransformation] = decl match
+	{
+		case (SINGULAR, DATIVE) => List(I_Umlaut)
+		case (PLURAL, NOMINATIVE | ACCUSATIVE) => List(I_Umlaut)
+		case _ => List.empty
+	}
+
 	override protected def inflection(decl: (Number, Case)) = decl match
 	{
-		case (SINGULAR, NOMINATIVE) => "r"
+		case (SINGULAR | PLURAL, NOMINATIVE) => "r"
 		case (SINGULAR, ACCUSATIVE)	=> ""
 		case (SINGULAR, DATIVE)			=> "i"
 		case (SINGULAR, GENITIVE)		=> "s"
 
-		case (PLURAL, NOMINATIVE)		=> "ar"
-		case (PLURAL, ACCUSATIVE | GENITIVE)		=> "a"
+		case (PLURAL, ACCUSATIVE)		=> "r"
+		case (PLURAL, GENITIVE)		=> "a"
 		case (PLURAL, DATIVE)		=> "um"
 	}
 }
