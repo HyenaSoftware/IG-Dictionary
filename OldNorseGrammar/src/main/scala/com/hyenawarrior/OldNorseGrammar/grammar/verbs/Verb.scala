7a9a24b71a38235f390654ecbfdd503fb46a5e1b
HyenaWarrior <andras.piroska.88@gmail.com>
2017-04-16 00:01:21 +0200

    Inherit the Verb class from Stem just like the Noun class

--- OldNorseGrammar/src/main/scala/com/hyenawarrior/OldNorseGrammar/grammar/verbs/Verb.scala
+++ OldNorseGrammar/src/main/scala/com/hyenawarrior/OldNorseGrammar/grammar/verbs/Verb.scala
@@ -1,11 +1,11 @@
 package com.hyenawarrior.OldNorseGrammar.grammar.verbs
 
-import com.hyenawarrior.OldNorseGrammar.grammar.{DescriptorFlag, PoS}
+import com.hyenawarrior.OldNorseGrammar.grammar.{DescriptorFlag, PoS, Stem}
 
 /**
 	* Created by HyenaWarrior on 2017.03.21..
 	*/
-class Verb(val str: String, val meaningId: Int) extends PoS
+class Verb(val str: String, val meaningId: Int) extends Stem(None) with PoS
 {
 	override def strForm = str
 
