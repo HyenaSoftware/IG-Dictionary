653e24c3d2ec885beb56026f6b758ca393338b1a
HyenaWarrior <andras.piroska.88@gmail.com>
2017-04-29 15:12:21 +0200

    Move save button to the meaning page. Add code to identify the index of the page

--- app/src/main/scala/com/example/hyenawarrior/myapplication/new_word/AddNewWordActivityPager.scala
+++ app/src/main/scala/com/example/hyenawarrior/myapplication/new_word/AddNewWordActivityPager.scala
@@ -14,48 +14,77 @@ class AddNewWordActivityPager extends AppCompatActivity
 {
 	outer =>
 
+	lazy val pager = findViewById(R.id.AddNewWordPager).asInstanceOf[ViewPager]
+
 	object AddWordPagerAdapter
 		extends FragmentStatePagerAdapter(outer.asInstanceOf[FragmentActivity].getSupportFragmentManager)
 	{
 		private val pages = List(AddNewWordActivity, SetMeaningFragment)
 
-		private var maxItem = 1
+		private var maxIndex = 0
+		private var currentIdx = 0
 
-		def allowToMoveForward(): Unit = {
+		def setNewPageIndex(nextIdx: Int): Unit = {
 
-			maxItem = maxItem + 1
+			/*if(nextIdx < currentIdx) {
+
+				maxIndex = nextIdx
+
+				notifyDataSetChanged()
+			}*/
+
+			currentIdx = nextIdx
+		}
+
+		def allowMoveToTheNextPage(): Int = {
+
+			maxIndex = min(currentIdx + 1, pages.length - 1)
 
 			notifyDataSetChanged()
+
+			maxIndex
 		}
 
 		override def getItem(position: Int): Fragment = pages(position)
 
-		override def getCount: Int = min(maxItem, pages.length)
+		override def getCount: Int = maxIndex + 1
+	}
+
+	/**
+		* Use this listener for
+		* - when user swipes back then disable the on-going pages
+		*/
+	object OnPageListener extends ViewPager.OnPageChangeListener {
+
+		override def onPageScrollStateChanged(state: Int): Unit = { }
+
+		override def onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int): Unit = { }
+
+		override def onPageSelected(position: Int): Unit = AddWordPagerAdapter.setNewPageIndex(position)
 	}
 
 	def selectThisDeclension(view: View): Unit = {
-		AddWordPagerAdapter.allowToMoveForward()
 
-		val lastPageIndex = AddWordPagerAdapter.getCount - 1
+		val nextPageIndex = AddWordPagerAdapter.allowMoveToTheNextPage()
 
-		val pager = findViewById(R.id.AddNewWordPager).asInstanceOf[ViewPager]
-		pager.setCurrentItem(lastPageIndex)
+		pager.setCurrentItem(nextPageIndex)
 	}
+
+	def saveWordToDatabase(view: View): Unit = finish()
 
 	def addNewOverride(view: View) = AddNewWordActivity.addNewOverride(view)
 
 	def onRemoveOverride(view: View) = AddNewWordActivity.onRemoveOverride(view)
 
 	//
-	protected override def onCreate(savedInstanceState: Bundle)
-	{
+	protected override def onCreate(savedInstanceState: Bundle): Unit = {
+
 		super.onCreate(savedInstanceState)
 
     setContentView(R.layout.activity_add_new_word_pager)
 
-		val fm = this.asInstanceOf[FragmentActivity]
-		val pager = findViewById(R.id.AddNewWordPager).asInstanceOf[ViewPager]
 		pager.setAdapter(AddWordPagerAdapter)
+		pager.addOnPageChangeListener(OnPageListener)
 
 		//https://developer.android.com/training/basics/firstapp/starting-activity.html
 		// Get the Intent that started this activity and extract the string
