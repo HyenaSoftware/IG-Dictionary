7ef81e75af63e43ad9b81d014b2f0de8d2cbe4bf
HyenaWarrior <andras.piroska.88@gmail.com>
2017-10-15 02:14:46 +0200

    Reorganize verb property dialog

--- app/src/main/scala/com/hyenawarrior/oldnorsedictionary/new_word/VerbDeclPreferencesDialog.scala
+++ app/src/main/scala/com/hyenawarrior/oldnorsedictionary/new_word/VerbDeclPreferencesDialog.scala
@@ -4,7 +4,7 @@ import android.app.{Activity, AlertDialog}
 import android.content.{Context, DialogInterface}
 import android.view.{LayoutInflater, View}
 import android.widget.AdapterView.OnItemSelectedListener
-import android.widget.{AdapterView, ArrayAdapter, Spinner}
+import android.widget.{AdapterView, ArrayAdapter, RadioButton, Spinner}
 import com.hyenawarrior.OldNorseGrammar.grammar.Pronoun
 import com.hyenawarrior.OldNorseGrammar.grammar.verbs.{VerbModeEnum, VerbTenseEnum, VerbType}
 import com.hyenawarrior.oldnorsedictionary.R
@@ -55,7 +55,9 @@ object VerbDeclPreferencesDialog
 	val spinnerItemsNonFinitive		=  List(
 			VerbForm.VERB_INFINITIVE.vtype,
 			VerbForm.VERB_PRESENT_PARTICIPLE.vtype,
-			VerbForm.VERB_PAST_PARTICIPLE.vtype,
+			VerbForm.VERB_PAST_PARTICIPLE.vtype)
+
+  val spinnerItemsImperative = List(
 			VerbForm.VERB_IMPERATIVE_SG_2ND.vtype,
 			VerbForm.VERB_IMPERATIVE_PL_1ST.vtype,
 			VerbForm.VERB_IMPERATIVE_PL_2ND.vtype)
@@ -73,30 +75,30 @@ class VerbDeclPreferencesDialog(activity: Activity)
 	var isReflexive = false
 	var callback: Option[Option[VerbType] => Unit] = None
 
-	object DialogOnClickListener extends DialogInterface.OnClickListener
-	{
-		override def onClick(dialogInterface: DialogInterface, i: Int): Unit = if(callback.isDefined) i match
-		{
+	object DialogOnClickListener extends DialogInterface.OnClickListener {
+
+		override def onClick(dialogInterface: DialogInterface, i: Int): Unit = if(callback.isDefined) i match {
+
 			case DialogInterface.BUTTON_POSITIVE => callback.get(Some(state))
 			case DialogInterface.BUTTON_NEGATIVE => callback.get(None)
 		}
 	}
 
-	object SpinnerListener extends OnItemSelectedListener
-	{
+	object SpinnerListener extends OnItemSelectedListener {
+
 		override def onNothingSelected(adapterView: AdapterView[_]): Unit = ???
 
-		override def onItemSelected(adapterView: AdapterView[_], view: View, idx: Int, l: Long): Unit =
-		{
+		override def onItemSelected(adapterView: AdapterView[_], view: View, idx: Int, l: Long): Unit = {
+
 			spinnerIndex = idx
-			resetState()
+			setState()
 		}
 	}
 
-	private def resetState(): Unit =
-	{
-		(activeVerbMode, activeVerbTense) match
-		{
+	private def setState(): Unit = {
+
+		(activeVerbMode, activeVerbTense) match {
+
 			case (R.id.rbInd, R.id.rbPresent) => state = spinnerItemsIndPresentItem(spinnerIndex)
 
 			case (R.id.rbInd, R.id.rbPreterite) => state = spinnerItemsIndPreteriteItem(spinnerIndex)
@@ -106,6 +108,8 @@ class VerbDeclPreferencesDialog(activity: Activity)
 			case (R.id.rbSubj, R.id.rbPreterite) => state = spinnerItemsSubjPreteriteItem(spinnerIndex)
 
 			case (R.id.rbNonFinitive, _) => state = spinnerItemsNonFinitive(spinnerIndex)
+
+      case (R.id.rbImperative, _) => state = spinnerItemsImperative(spinnerIndex)
 		}
 	}
 
@@ -114,38 +118,64 @@ class VerbDeclPreferencesDialog(activity: Activity)
 		val verbDeclSpinner: Spinner = selfView.findViewById(R.id.spVerbDeclensions).asInstanceOf[Spinner]
 		verbDeclSpinner.setOnItemSelectedListener(SpinnerListener)
 
-		override def onClick(view: View): Unit = view.getId match
-		{
-			case R.id.rbInd | R.id.rbSubj =>
-				activeVerbMode = view.getId
+		override def onClick(view: View): Unit = view.getId match {
+
+			case id @ (R.id.rbInd | R.id.rbSubj) =>
+				activeVerbMode = id
 				setSpinnerAndTenseRadioButtons(R.array.verb_ind_subj_decls, true)
-				resetState()
+        disableOtherRadioButtonsThan(id)
+				setState()
 
-			case R.id.rbNonFinitive				=>
-				activeVerbMode = view.getId
+			case R.id.rbNonFinitive =>
+				activeVerbMode = R.id.rbNonFinitive
 				setSpinnerAndTenseRadioButtons(R.array.verb_nonfinitive_decls, false)
-				resetState()
+        disableOtherRadioButtonsThan(R.id.rbNonFinitive)
+				setState()
 
-			case R.id.rbPresent | R.id.rbPreterite =>
-				activeVerbTense = view.getId
-				resetState()
+      case R.id.rbImperative =>
+        activeVerbMode = R.id.rbImperative
+
+        rbPresent.setChecked(true)
+        rbPreterite.setChecked(false)
+
+        setSpinnerAndTenseRadioButtons(R.array.verb_imperative_decls, false)
+        disableOtherRadioButtonsThan(R.id.rbImperative)
+        setState()
+
+			case id @ (R.id.rbPresent | R.id.rbPreterite) =>
+				activeVerbTense = id
+				setState()
 		}
 
-		private def setSpinnerAndTenseRadioButtons(intSpinnerRsrc: Int, enableTenseButtons: Boolean): Unit =
-		{
+		private def setSpinnerAndTenseRadioButtons(intSpinnerRsrc: Int, enableTenseButtons: Boolean): Unit = {
+
 			val adapter = ArrayAdapter.createFromResource(activity,	intSpinnerRsrc, android.R.layout.simple_spinner_item)
 			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 			verbDeclSpinner.setAdapter(adapter)
 
-			selfView.findViewById(R.id.rbPresent).setEnabled(enableTenseButtons)
-			selfView.findViewById(R.id.rbPreterite).setEnabled(enableTenseButtons)
+      rbPresent.setEnabled(enableTenseButtons)
+			rbPreterite.setEnabled(enableTenseButtons)
 		}
+
+    private def disableOtherRadioButtonsThan(selectedRb: Int): Unit = {
+
+      val otherRbs = Seq(R.id.rbInd, R.id.rbSubj, R.id.rbImperative, R.id.rbNonFinitive)
+        .filterNot(_ == selectedRb)
+
+      for(id <- otherRbs) {
+
+        selfView.findViewById(id) match {
+          case rb: RadioButton => rb.setChecked(false)
+          case _ => ()
+        }
+      }
+    }
 	}
 
-	def show(callback: VerbType => Unit)
-	{
-		this.callback = Some
-		{
+	def show(callback: VerbType => Unit) {
+
+		this.callback = Some {
+
 			case Some(v) => callback(v)
 			case _ => ()
 		}
@@ -153,22 +183,25 @@ class VerbDeclPreferencesDialog(activity: Activity)
 		dialog.show()
 	}
 
-	def showOpt(callback: Option[VerbType] => Unit)
-	{
+	def showOpt(callback: Option[VerbType] => Unit) {
+
 		this.callback = Some(callback)
 
 		dialog.show()
 	}
 
-	private val selfView =
-	{
+	private val selfView = {
+
 		val inflater = activity.getSystemService(Context.LAYOUT_INFLATER_SERVICE).asInstanceOf[LayoutInflater]
 
 		inflater.inflate(R.layout.new_verb_overriding_def_row_preferences, null)
 	}
 
+  val rbPresent = selfView.findViewById(R.id.rbPresent).asInstanceOf[RadioButton]
+  val rbPreterite = selfView.findViewById(R.id.rbPreterite).asInstanceOf[RadioButton]
+
 	// wire the radio buttons to the event handler
-	for(id <- Seq(R.id.rbInd, R.id.rbSubj, R.id.rbNonFinitive, R.id.rbPresent, R.id.rbPreterite))
+	for(id <- Seq(R.id.rbInd, R.id.rbSubj, R.id.rbImperative, R.id.rbNonFinitive, R.id.rbPresent, R.id.rbPreterite))
 	{
 		selfView.findViewById(id).setOnClickListener(RadioButtonListener)
 	}
