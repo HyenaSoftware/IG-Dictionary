59f743772e7e3ca112e0cda29f3fd5bcb7817fe5
HyenaWarrior <andras.piroska.88@gmail.com>
2017-12-09 13:44:25 +0100

    Remove unused method.

--- OldNorseGrammarEngine/src/main/scala/com/hyenawarrior/OldNorseGrammar/grammar/verbs/package.scala
+++ OldNorseGrammarEngine/src/main/scala/com/hyenawarrior/OldNorseGrammar/grammar/verbs/package.scala
@@ -1,8 +1,6 @@
 package com.hyenawarrior.OldNorseGrammar.grammar
 
 import com.hyenawarrior.OldNorseGrammar.grammar.GNumber.{PLURAL, SINGULAR}
-import com.hyenawarrior.OldNorseGrammar.grammar.morphophonology.{Ablaut, StaticAblaut}
-import com.hyenawarrior.OldNorseGrammar.grammar.verbs.VerbClassEnum._
 import com.hyenawarrior.OldNorseGrammar.grammar.verbs.VerbModeEnum._
 import com.hyenawarrior.OldNorseGrammar.grammar.verbs.VerbTenseEnum._
 import com.hyenawarrior.OldNorseGrammar.grammar.verbs.stem.EnumVerbStem
@@ -48,19 +46,5 @@ package object verbs
 		case PRESENT_STEM							=> PRESENT	-> None
 		case PRETERITE_SINGULAR_STEM	=> PAST			-> Some(SINGULAR)
 		case PRETERITE_PLURAL_STEM		=> PAST			-> Some(PLURAL)
-	}
-
-	def getDescOfStrongVerbClassFor(verbClass: StrongVerbClassEnum, verbForms: Map[EnumVerbStem, Seq[String]] = Map()): Option[StrongVerbClassDesc]
-		= verbClass match
-	{
-		case STRONG_1ST_CLASS  => Some(StrongVerbClassDesc(STRONG_1ST_CLASS, StaticAblaut("í",	"ei", "i", "i")))
-		case STRONG_2ND_CLASS  => Some(StrongVerbClassDesc(STRONG_2ND_CLASS, StaticAblaut("jú", "au", "u", "o")))
-		case STRONG_3RD_CLASS  => Some(StrongVerbClassDesc(STRONG_3RD_CLASS, StaticAblaut("e", 	"a", 	"u", "o")))
-
-		case STRONG_4TH_CLASS  => Some(StrongVerbClassDesc(STRONG_4TH_CLASS, StaticAblaut("e",	"a", 	"á", "o")))
-		case STRONG_5TH_CLASS  => Some(StrongVerbClassDesc(STRONG_5TH_CLASS, StaticAblaut("e", 	"a", 	"á", "e")))
-		case STRONG_6TH_CLASS  => Some(StrongVerbClassDesc(STRONG_6TH_CLASS, StaticAblaut("a", 	"ó", 	"ó", "a")))
-
-		case STRONG_7TH_CLASS => Ablaut.extractAblautFrom(verbForms).map(a => StrongVerbClassDesc(STRONG_7TH_CLASS, a))
 	}
 }
