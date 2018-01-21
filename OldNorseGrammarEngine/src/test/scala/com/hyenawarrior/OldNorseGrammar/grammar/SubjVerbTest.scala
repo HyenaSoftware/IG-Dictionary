f4e766a1fa7abeb6e06ef71dd6e8b40d54903471
HyenaWarrior <andras.piroska.88@gmail.com>
2018-01-21 17:26:46 +0100

    More testcases for subjunctive verb conjugation

--- OldNorseGrammarEngine/src/test/scala/com/hyenawarrior/OldNorseGrammar/grammar/SubjVerbTest.scala
+++ OldNorseGrammarEngine/src/test/scala/com/hyenawarrior/OldNorseGrammar/grammar/SubjVerbTest.scala
@@ -1,9 +1,9 @@
 package com.hyenawarrior.OldNorseGrammar.grammar
 
-import com.hyenawarrior.OldNorseGrammar.grammar.verbs.{StrongVerb, VerbClassEnum, VerbType, VerbVoice}
 import com.hyenawarrior.OldNorseGrammar.grammar.verbs.VerbModeEnum._
 import com.hyenawarrior.OldNorseGrammar.grammar.verbs.VerbTenseEnum._
 import com.hyenawarrior.OldNorseGrammar.grammar.verbs.VerbVoice.ACTIVE
+import com.hyenawarrior.OldNorseGrammar.grammar.verbs.{StrongVerb, VerbClassEnum, VerbType}
 import org.junit.Assert._
 import org.junit.Test
 
@@ -36,5 +36,40 @@ class SubjVerbTest {
     assertEquals("skytim",  verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PAST), Some(Pronoun.PL_1)).strForm)
     assertEquals("skytið",  verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PAST), Some(Pronoun.PL_2)).strForm)
     assertEquals("skyti",   verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PAST), Some(Pronoun.PL_3)).strForm)
+  }
+
+  def testClass5thVerbStressShift(): Unit = {
+
+    val inf = (INFINITIVE, ACTIVE, None, None)
+
+    val verb = StrongVerb(VerbClassEnum.STRONG_5TH_CLASS, Map(inf -> "sjá"))
+
+    assertEquals("sé",  verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PRESENT), Some(Pronoun.SG_1)).strForm)
+    assertEquals("sér", verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PRESENT), Some(Pronoun.SG_3)).strForm)
+    assertEquals("sém", verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PRESENT), Some(Pronoun.PL_1)).strForm)
+
+    assertEquals("sæir", verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PAST), Some(Pronoun.SG_2)).strForm)
+    assertEquals("sæi",  verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PAST), Some(Pronoun.SG_3)).strForm)
+
+    assertEquals("sæim", verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PAST), Some(Pronoun.PL_1)).strForm)
+    assertEquals("sæi",  verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PAST), Some(Pronoun.PL_3)).strForm)
+  }
+
+  @Test
+  def testClass5thVerb(): Unit = {
+
+    val inf = (INFINITIVE, ACTIVE, None, None)
+
+    val verb = StrongVerb(VerbClassEnum.STRONG_5TH_CLASS, Map(inf -> "reka"))
+
+    assertEquals("reka",  verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PRESENT), Some(Pronoun.SG_1)).strForm)
+    assertEquals("reki", verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PRESENT), Some(Pronoun.SG_3)).strForm)
+    assertEquals("rekim", verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PRESENT), Some(Pronoun.PL_1)).strForm)
+
+    assertEquals("rækir", verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PAST), Some(Pronoun.SG_2)).strForm)
+    assertEquals("ræki",  verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PAST), Some(Pronoun.SG_3)).strForm)
+
+    assertEquals("rækim", verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PAST), Some(Pronoun.PL_1)).strForm)
+    assertEquals("ræki",  verb.verbForms(SUBJUNCTIVE, ACTIVE, Some(PAST), Some(Pronoun.PL_3)).strForm)
   }
 }
