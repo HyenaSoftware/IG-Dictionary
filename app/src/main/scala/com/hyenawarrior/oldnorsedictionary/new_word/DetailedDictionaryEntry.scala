49fd54fb9da66b4d77bf851c97d2a158f3a17d5d
HyenaWarrior <andras.piroska.88@gmail.com>
2018-01-27 15:34:09 +0100

    Add buttons to collapse the panels in the detailed view panel.

--- app/src/main/scala/com/hyenawarrior/oldnorsedictionary/new_word/DetailedDictionaryEntry.scala
+++ app/src/main/scala/com/hyenawarrior/oldnorsedictionary/new_word/DetailedDictionaryEntry.scala
@@ -1,9 +1,11 @@
 package com.hyenawarrior.oldnorsedictionary.new_word
 
 import android.os.Bundle
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.widget.{Adapter, LinearLayout}
-import com.hyenawarrior.OldNorseGrammar.grammar.verbs.{StrongVerb, VerbModeEnum, VerbVoice}
+import com.hyenawarrior.OldNorseGrammar.grammar.verbs.StrongVerb
 import com.hyenawarrior.oldnorsedictionary.R
 import com.hyenawarrior.oldnorsedictionary.model.DictionaryListItem
 import com.hyenawarrior.oldnorsedictionary.modelview._
@@ -52,5 +54,32 @@ class DetailedDictionaryEntry extends AppCompatActivity {
       //
       val view = findViewById(R.id.verb_conjugation_viewer)
       setDeclensionsTo(sv, view)
+  }
+
+  def onCollapseView(view: View): Unit = {
+
+    val picRsrc = view.getId match {
+
+      case R.id.iv_toggle_active_voice_view       => invertVisibilityAndGetNewPicRsrc(R.id.ll_active_voice_view)
+      case R.id.iv_toggle_mediopassive_voice_view => invertVisibilityAndGetNewPicRsrc(R.id.ll_mediopassive_voice_view)
+      case R.id.iv_toggle_past_participle_view    => invertVisibilityAndGetNewPicRsrc(R.id.ll_past_participle_view)
+    }
+
+    val pic = ContextCompat.getDrawable(getApplicationContext, picRsrc)
+    view setBackground pic
+  }
+
+  private def invertVisibilityAndGetNewPicRsrc(viewId: Int): Int = {
+
+    val panel = findViewById(viewId)
+    val (nv, picRsrc) = panel.getVisibility match {
+
+      case View.VISIBLE => View.GONE -> R.drawable.ic_keyboard_arrow_down_black_18dp
+      case View.GONE => View.VISIBLE -> R.drawable.ic_keyboard_arrow_up_black_18dp
+    }
+
+    panel setVisibility nv
+
+    picRsrc
   }
 }
