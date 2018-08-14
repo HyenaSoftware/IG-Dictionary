3d8cfd629846e2fe33fbc4689e79470414f23972
HyenaWarrior <andras.piroska.88@gmail.com>
2018-08-14 22:48:35 +0200

    Add support for traditional syllabification

--- OldNorseGrammarEngine/src/main/scala/com/hyenawarrior/OldNorseGrammar/grammar/Syllables.scala
+++ OldNorseGrammarEngine/src/main/scala/com/hyenawarrior/OldNorseGrammar/grammar/Syllables.scala
@@ -2,6 +2,9 @@ package com.hyenawarrior.OldNorseGrammar.grammar
 
 import com.hyenawarrior.OldNorseGrammar.grammar.phonology.Vowel._
 import com.hyenawarrior.OldNorseGrammar.grammar.Syllable.lengthOf
+import com.hyenawarrior.OldNorseGrammar.grammar.phonology.Consonant
+
+import scala.annotation.tailrec
 
 /**
   * Created by HyenaWarrior on 2018.06.09..
@@ -66,6 +69,101 @@ object Syllables {
 
     Some(adjustAndCraeteSyllables(firstOnset, parts, isStressed = true))
   }
+
+
+
+  trait MODE { def syllablesOf(word: String): List[Syllable] }
+
+  object METRICAL extends MODE {
+
+    override def syllablesOf(word: String): List[Syllable] = ???
+  }
+
+  object TRADITIONAL_IMP extends MODE {
+
+    def syllablesOf(word: String): List[Syllable] = {
+
+      // 0,4,6
+      val syllableBegins = 0 +: word.zipWithIndex
+        .filter { case (c, _) =>  isVowel(c) }
+        .map { case (_, i) => i }
+        .tail
+
+      // 4,6
+      val syllableBeginsShifted = syllableBegins.tail :+ word.length
+
+      // (0,4), (4,6), (6,7)
+      val syllableRanges = syllableBegins zip syllableBeginsShifted
+
+      //
+      val syllables = syllableRanges.map {
+
+        case (0, e) => null
+        case (s, e) =>
+        val longestNucleus = word.substring(s, s + 2)
+        val nucleus = if(isDiphtong(longestNucleus)) longestNucleus else word.substring(s, s + 1)
+        val coda = word.substring(s + nucleus.length - 1, e)
+        new Syllable("", nucleus, coda, true, Syllable.lengthOf(nucleus, coda))
+      }
+
+      syllables.toList
+    }
+  }
+
+  object TRADITIONAL_REC extends MODE {
+
+    val ONSET = 0
+    val NUCLEUS = 1
+    val CODA = 2
+
+    def saveAndBuildSyllable(parts: List[String], is: Int): Syllable = {
+
+      val coda = parts.lift(0).getOrElse("")
+      val nucleus = parts.lift(1).getOrElse("")
+      val onset = parts.lift(2).getOrElse("")
+      val length = Syllable.lengthOf(nucleus, coda)
+
+      new Syllable(onset, nucleus, coda, onset.nonEmpty || is == 0, length)
+    }
+
+    def concat(str: String, is: Int, ic: Int, parts: List[String]): List[String] = str.substring(is, ic) +: parts
+
+    // kall-að-i
+    @tailrec
+    def processNextChar(str: String, is: Int, ic: Int, parts: List[String], syllables: List[Syllable], mode: Int): List[Syllable] = {
+
+      val c = str(ic)
+
+      val inTheCorrectMode = mode match {
+
+        case ONSET | CODA => Consonant.isConsonant(c)
+        case NUCLEUS => isVowel(c)
+      }
+
+      (mode, inTheCorrectMode) match {
+
+        case _ if c == '$' =>
+          saveAndBuildSyllable(concat(str, is, ic, parts), is) :: syllables
+
+        // normal character processing
+        case (_, true) => processNextChar(str, is, ic + 1, parts, syllables, mode)
+
+        //
+        case (ONSET,    false) => processNextChar(str, ic, ic, concat(str, is, ic, parts), syllables, NUCLEUS)
+        case (NUCLEUS,  false) => processNextChar(str, ic, ic, concat(str, is, ic, parts), syllables, CODA)
+
+        case (CODA, false) =>
+          val sy = saveAndBuildSyllable(concat(str, is, ic, parts), is)
+          processNextChar(str, ic, ic, List(), sy :: syllables, NUCLEUS)
+
+        case _ => syllables
+      }
+    }
+
+    def syllablesOf(word: String): List[Syllable] = processNextChar(word + '$', 0, 0, List(), List(), ONSET)
+  }
+
+  def syllablesOf(word: String, mode: MODE = TRADITIONAL_REC): List[Syllable] = mode.syllablesOf(word)
 
   def apply(syllables: List[Syllable]): String = syllables.flatMap(_.letters).mkString
 }
