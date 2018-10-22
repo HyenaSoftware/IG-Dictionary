ce165d4d7bf3ff44fa828518b86cf50346ab1a6b
HyenaWarrior <andras.piroska.88@gmail.com>
2018-10-22 21:49:25 +0200

    Code cleanup

--- app/src/main/scala/com/hyenawarrior/oldnorsedictionary/modelview/add_new_word_panel/NounDeclensionAdapter.scala
+++ app/src/main/scala/com/hyenawarrior/oldnorsedictionary/modelview/add_new_word_panel/NounDeclensionAdapter.scala
@@ -3,8 +3,7 @@ package com.hyenawarrior.oldnorsedictionary.modelview.add_new_word_panel
 import android.app.Activity
 import android.view.{View, ViewGroup}
 import android.widget.Button
-import com.hyenawarrior.OldNorseGrammar.grammar.enums.Case.{unapply => _, _}
-import com.hyenawarrior.OldNorseGrammar.grammar.enums.GNumber._
+import com.hyenawarrior.OldNorseGrammar.grammar.enums.Case.{unapply => _}
 import com.hyenawarrior.OldNorseGrammar.grammar.nouns.Noun
 import com.hyenawarrior.OldNorseGrammar.grammar.nouns.stemclasses.enum.NounStemClassEnum
 import com.hyenawarrior.oldnorsedictionary.R
@@ -13,21 +12,6 @@ import com.hyenawarrior.oldnorsedictionary.modelview.{CustomAdapter, setDeclensi
 /**
 	* Created by HyenaWarrior on 2017.04.12..
 	*/
-object NounDeclensionAdapter
-{
-	val NOUN_EDIT_TEXTS = List(
-		(R.id.tvNewWord_Nom_Sg, 	(SINGULAR, NOMINATIVE))
-		, (R.id.tvNewWord_Acc_Sg, (SINGULAR, ACCUSATIVE))
-		, (R.id.tvNewWord_Dat_Sg, (SINGULAR, DATIVE))
-		, (R.id.tvNewWord_Gen_Sg, (SINGULAR, GENITIVE))
-
-		, (R.id.tvNewWord_Nom_Pl, (PLURAL, NOMINATIVE))
-		, (R.id.tvNewWord_Acc_Pl, (PLURAL, ACCUSATIVE))
-		, (R.id.tvNewWord_Dat_Pl, (PLURAL, DATIVE))
-		, (R.id.tvNewWord_Gen_Pl, (PLURAL, GENITIVE))
-	)
-}
-
 class NounDeclensionAdapter(activity: Activity, listView: ViewGroup)
   extends CustomAdapter[(NounStemClassEnum, Noun)](activity, listView, R.layout.noun_declension)
 {
